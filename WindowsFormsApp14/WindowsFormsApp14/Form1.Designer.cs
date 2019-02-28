97720ee2af39137ba3971ae4304e523d6a8482a4
eee24 <48090853+eee24@users.noreply.github.com>
2019-02-28 07:01:36 -0500

    Add files via upload

---
+++ WindowsFormsApp14/WindowsFormsApp14/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace WindowsFormsApp14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(121, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Orange;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.ForeColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(103, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Orange;
+            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(94, 160);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.Orange;
+            this.listBox2.ForeColor = System.Drawing.Color.Red;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(121, 68);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(99, 160);
+            this.listBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(121, 234);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(12, 263);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(208, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "start";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(206, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "x";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(192, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "-";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(60, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Origin Checker";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = global::WindowsFormsApp14.Properties.Resources.Cornmanthe3rd_Metronome_Other_origin;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 34);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(12, 292);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(208, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "credit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(230, 323);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp14/WindowsFormsApp14/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace WindowsFormsApp14
+{
+    public partial class Form1 : Form
+    {
+        private bool mouseIsDown = false;
+        private Point firstPoint;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.AddRange(File.ReadAllLines(openFileDialog1.FileName));
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            const string sPath = "available.txt";
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sPath);
+            foreach (var item in listBox1.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            const string sPath = "taken.txt";
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sPath);
+            foreach (var item in listBox2.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+        }
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "start")
+            {
+                timer1.Start();
+                button5.Text = "stop";
+                
+            }
+            else if (button5.Text == "stop")
+            {
+                timer1.Stop();
+                button5.Text = "start";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient steam = new WebClient();
+                string check = steam.DownloadString("https://signin.ea.com/p/ajax/user/checkOriginId?requestorId=portal&originId=" + comboBox1.SelectedItem.ToString());
+                string input = check;
+                string taken = @"status(.+?)origin_id_duplicated";
+                string open = @"status(.+?)true";
+                string vulgar = @"message(.+?)origin_id_not_allowed";
+                string small = @"message(.+?)origin_id_too_short";
+                string big = @"message(.+?)origin_id_too_long";
+                RegexOptions options = RegexOptions.Multiline;
+                if (Regex.IsMatch(input, taken, options))
+                {
+                    listBox2.Items.Add(comboBox1.SelectedItem.ToString());
+                    if (comboBox1.SelectedIndex == (comboBox1.Items.Count - 1))
+                    {
+                        comboBox1.SelectedIndex = 0;
+                        timer1.Stop();
+                    }
+                    int selectedIndex = comboBox1.SelectedIndex;
+                    comboBox1.SelectedIndex = selectedIndex + 1;
+                    listBox2.TopIndex = listBox2.Items.Count - 1;
+
+                }
+                 if (Regex.IsMatch(input, open, options))
+                {
+                    listBox1.Items.Add(comboBox1.SelectedItem.ToString());
+                    if (comboBox1.SelectedIndex == (comboBox1.Items.Count - 1))
+                    {
+                        comboBox1.SelectedIndex = 0;
+                        timer1.Stop();
+                    }
+                    int selectedIndex = comboBox1.SelectedIndex;
+                    comboBox1.SelectedIndex = selectedIndex + 1;
+                    listBox1.TopIndex = listBox1.Items.Count - 1;
+                }
+                if (Regex.IsMatch(input, vulgar, options))
+                {
+                    listBox2.Items.Add(comboBox1.SelectedItem.ToString());
+                    if (comboBox1.SelectedIndex == (comboBox1.Items.Count - 1))
+                    {
+                        comboBox1.SelectedIndex = 0;
+                        timer1.Stop();
+                    }
+                    int selectedIndex = comboBox1.SelectedIndex;
+                    comboBox1.SelectedIndex = selectedIndex + 1;
+                    listBox2.TopIndex = listBox2.Items.Count - 1;
+                }
+                if (Regex.IsMatch(input, small, options))
+                {
+                    listBox2.Items.Add(comboBox1.SelectedItem.ToString());
+                    if (comboBox1.SelectedIndex == (comboBox1.Items.Count - 1))
+                    {
+                        comboBox1.SelectedIndex = 0;
+                        timer1.Stop();
+                    }
+                    int selectedIndex = comboBox1.SelectedIndex;
+                    comboBox1.SelectedIndex = selectedIndex + 1;
+                    listBox2.TopIndex = listBox2.Items.Count - 1;
+                }
+                if (Regex.IsMatch(input, big, options))
+                {
+                    listBox2.Items.Add(comboBox1.SelectedItem.ToString());
+                    if (comboBox1.SelectedIndex == (comboBox1.Items.Count - 1))
+                    {
+                        comboBox1.SelectedIndex = 0;
+                        timer1.Stop();
+                    }
+                    int selectedIndex = comboBox1.SelectedIndex;
+                    comboBox1.SelectedIndex = selectedIndex + 1;
+                    listBox2.TopIndex = listBox2.Items.Count - 1;
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError &&
+                    ex.Response != null)
+                {
+                    var resp = (HttpWebResponse)ex.Response;
+                    if (resp.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        listBox1.Items.Add(comboBox1.SelectedItem.ToString());
+                        if (comboBox1.SelectedIndex == (comboBox1.Items.Count - 1))
+                        {
+                            comboBox1.SelectedIndex = 0;
+                            timer1.Stop();
+                        }
+                        int selectedIndex = comboBox1.SelectedIndex;
+                        comboBox1.SelectedIndex = selectedIndex + 1;
+                        listBox1.TopIndex = listBox1.Items.Count - 1;
+
+                    }
+                }
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            firstPoint = e.Location;
+            mouseIsDown = true;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseIsDown)
+            {
+                int xDiff = firstPoint.X - e.Location.X;
+                int yDiff = firstPoint.Y - e.Location.Y;
+                int x = this.Location.X - xDiff;
+                int y = this.Location.Y - yDiff;
+                this.Location = new Point(x, y);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseIsDown = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=SR1vGzEiv90");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("coded and created by @eric", "Origin Checker");
+        }
+    }
+ }
